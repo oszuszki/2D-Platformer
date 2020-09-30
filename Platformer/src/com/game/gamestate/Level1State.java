4e09b6dc8838e7cf43866019a39254ab5f8a5a17
Uszu <50295110+oszuszki@users.noreply.github.com>
2020-10-01 00:42:59 +0300

    Level1State fejlesztve

--- Platformer/src/com/game/gamestate/Level1State.java
+++ Platformer/src/com/game/gamestate/Level1State.java
@@ -1,6 +1,7 @@
 package com.game.gamestate;
 
 import com.game.entities.Player;
+import com.game.mapping.Map;
 import com.game.objects.Block;
 
 import java.awt.Graphics;
@@ -8,8 +9,7 @@ import java.awt.Graphics;
 public class Level1State extends GameState{
 
     private Player player;
-
-    private Block[] b;
+    private Map map;
 
     public Level1State(GameStateManager gsm) {
         super(gsm);
@@ -17,33 +17,23 @@ public class Level1State extends GameState{
 
     @Override
     public void init() {
-
         player = new Player(30, 30);
-        b= new Block[9];
-        b[0]=new Block(100,400);
-        b[1]=new Block(200,500);
-        b[2]=new Block(300,500);
-        b[3]=new Block(400,500);
-        b[4]=new Block(500,400);
-        b[5]=new Block(600,400);
-        b[6]=new Block(700,300);
-        b[7]=new Block(900,300);
-        b[8]=new Block(600,200);
+        map = new Map("",4, 4);
 
+        xOffset = -200;
+        yOffset = -400;
     }
 
     @Override
     public void tick() {
-        player.tick(b);
+
+        player.tick(map.getBlocks());
     }
 
     @Override
     public void draw(Graphics g) {
-
         player.draw(g);
-        for (int i=0 ; i<b.length;i++){
-            b[i].draw(g);
-        }
+        map.draw(g);
     }
 
     @Override
