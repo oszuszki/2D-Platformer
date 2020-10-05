afe0aaa5c9263527db03ffbdaa23c53686114b0f
Uszu <50295110+oszuszki@users.noreply.github.com>
2020-10-05 16:26:25 +0300

    Background

--- Platformer/src/com/game/resources/Images.java
+++ Platformer/src/com/game/resources/Images.java
@@ -2,8 +2,6 @@ package com.game.resources;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -11,12 +9,12 @@ import javax.imageio.ImageIO;
 public class Images {
 
 	public static BufferedImage[] blocks;
-	public static BufferedImage[] sky;
 	
 	public Images() {
-		blocks = new BufferedImage[1];
+		blocks = new BufferedImage[2];
 		try {
-			blocks[0] = ImageIO.read(getClass().getResourceAsStream("res/Blocks/pixelbrick.png"));
+			blocks[0] = ImageIO.read(getClass().getResourceAsStream("/res/Blocks/pixelbrick.png"));
+			blocks[1] = ImageIO.read(getClass().getResourceAsStream("/res/Blocks/sky.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
